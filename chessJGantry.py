2c8b983c528421f0293a0eddaf2ae1cb9e5f8415
Joël Vermeulen <98051242+JoelVerm@users.noreply.github.com>
2023-01-12 08:46:04 +0100

    gantry done

--- chessJGantry.py
+++ chessJGantry.py
@@ -1,10 +1,10 @@
 import threading
 from time import sleep
-from gpiozero import OutputDevice
+from gpiozero import OutputDevice, Button
 
 
 class Stepper:
-    def __init__(self, pin1, pin2, pin3, pin4):
+    def __init__(self, pin1: int, pin2: int, pin3: int, pin4: int):
         self.motorPins = [
             OutputDevice(pin1),
             OutputDevice(pin2),
@@ -21,6 +21,9 @@ class Stepper:
             [0, 0, 1, 1],
             [0, 0, 0, 1]
         ]
+        self.step = 0
+        self.steps = len(self.sequence)
+        self.pointsDone = 0
         self.running = False
         self.mustRun = False
         self.thread = None
@@ -29,8 +32,9 @@ class Stepper:
         if self.running and self.thread:
             self.mustRun = False
             self.thread.join()
+        return self.pointsDone
 
-    def move(self, points, speed):
+    def move(self, points: int, speed: int):
         if self.running:
             self.stop()
         if points == 0:
@@ -39,48 +43,98 @@ class Stepper:
         self.running = True
         self.mustRun = True
         self.thread = threading.Thread(
-            target=self.moveThreaded, args=(points, speed))
+            target=self.moveSync, args=(points, speed))
         self.thread.daemon = True
         self.thread.start()
 
-    def moveThreaded(self, points, speed):
+    def moveSync(self, points: int, speed: int):
         delay = 1 / speed
         direction = 1 if points >= 0 else -1
-        pointsDone = 0
+        self.pointsDone = 0
         pointsAbs = abs(points)
-        step = 0
-        steps = len(self.sequence)
         while self.mustRun:
             for i, pin in enumerate(self.motorPins):
-                if self.sequence[step][i]:
+                if self.sequence[self.step][i]:
                     pin.on()
                 else:
                     pin.off()
             sleep(delay)
-            step += direction
-            if step >= steps:
-                step = 0
-            if step < 0:
-                step = steps - 1
-            pointsDone += 1
-            if pointsDone >= pointsAbs:
+            self.step += direction
+            if self.step >= self.steps:
+                self.step = 0
+            if self.step < 0:
+                self.step = self.steps - 1
+            self.pointsDone += 1
+            if self.pointsDone >= pointsAbs:
                 break
         self.running = False
 
 
 class Gantry:
-    def __init__(self, motor1pins=[1, 2, 3, 4], motor2pins=[5, 6, 7, 8]):
-        self.stepper1 = Stepper(*motor1pins)
-        self.stepper2 = Stepper(*motor2pins)
-        self.speed = 5
+    def __init__(self, stepper1: Stepper, stepper2: Stepper, stopper11: Button, stopper12: Button, stopper21: Button, stopper22: Button, speed=5):
+        self.stepper1 = stepper1
+        self.stepper2 = stepper2
+        self.stopper11 = stopper11
+        self.stopper12 = stopper12
+        self.stopper21 = stopper21
+        self.stopper22 = stopper22
+        self.speed = speed
 
-    def move(self, percentX, percentY):
-        pass
+    def initialize(self):
+        speed = 2
+        self.stepper1.move(-10e30, speed)
+        self.stopper11.wait_for_active()
+        self.stepper1.stop()
+        self.stepper1.move(10e30, speed)
+        self.stopper12.wait_for_active()
+        self.rangeX = self.stepper1.stop()
+        self.posX = self.rangeX
+        self.stepper2.move(-10e30, speed)
+        self.stopper21.wait_for_active()
+        self.stepper2.stop()
+        self.stepper2.move(10e30, speed)
+        self.stopper22.wait_for_active()
+        self.rangeY = self.stepper2.stop()
+        self.posY = self.rangeY
+        self.move(0, 0)
+
+    def move(self, percentX: int, percentY: int):
+        if not (0 <= percentX <= 100 and 0 <= percentY <= 100):
+            raise ValueError('percentX and percentY must be in range [0, 100]')
+        pointsX = self.rangeX * (percentX / 100)
+        pointsY = self.rangeY * (percentY / 100)
+        moveX = pointsX - self.posX
+        moveY = pointsY - self.posY
+        self.stepper1.move(moveX, self.speed)
+        self.stepper2.move(moveY, self.speed)
+        while self.stepper1.running or self.stepper2.running:
+            sleep(0.05)
+        self.posX += moveX
+        self.posY += moveY
 
 
-def main():
-    pass
+class ChessBoardGantry:
+    def __init__(self, gantry: Gantry, magnet: OutputDevice):
+        self.gantry = gantry
+        self.magnet = magnet
+        gantry.initialize()
+
+    def toPercentage(self, cell: int):
+        return (100 // 16) + cell * (100 // 8)
+
+    def moveToCell(self, cellX: int, cellY: int):
+        percentX = self.toPercentage(cellX)
+        percentY = self.toPercentage(cellY)
+        self.gantry.move(percentX, percentY)
 
 
-if __name__ == '__main__':
-    main()
+def createGantry():
+    return ChessBoardGantry(
+        Gantry(
+            Stepper(1, 2, 3, 4),
+            Stepper(5, 6, 7, 8),
+            Button(9),
+            Button(10),
+            Button(11),
+            Button(12)),
+        OutputDevice(13))
